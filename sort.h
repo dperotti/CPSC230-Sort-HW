8fde11bb91e30be0bccc9b823040c0476eaf9136
dperotti <domincperotti@gmail.com>
2014-01-05 22:48:10 -0500

    Header File

---
+++ sort.h
@@ -0,0 +1,15 @@
+
+// cpsc230  sort unit test
+
+#ifndef GTEST_SAMPLES_SAMPLE1_H_
+#define GTEST_SAMPLES_SAMPLE1_H_
+
+
+#include<vector>
+using namespace std;
+vector<int> bubbleSort(vector<int>);
+vector<int> selectionSort(vector<int>);
+vector<int> mergeSort(vector<int>);
+
+
+#endif  // GTEST_SAMPLES_SAMPLE1_H_
